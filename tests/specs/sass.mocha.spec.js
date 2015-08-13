1c42f243991db065306815f474944d8f4f88cd82
anolilab <info@anolilab.de>
2015-08-13 10:01:00 +0200

    added test for node-sass and ruby-sass

--- tests/specs/sass.mocha.spec.js
+++ tests/specs/sass.mocha.spec.js
@@ -2,6 +2,6 @@
 
 var path     = require('path'),
     sassTrue = require('sass-true'),
-    sassFile = path.join(__dirname, 'tests.scss');
+    sassFile = path.join(__dirname, 'node-tests.scss');
 
 sassTrue.runSass({file: sassFile}, describe, it);
