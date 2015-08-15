d5dab55721e495cece6058a50a5684e1bad2d7e5
Daniel Bannert <d.bannert@anolilab.de>
2015-08-15 23:16:15 +0200

    Added new tests and some new funcs

--- tests/specs/sass.mocha.spec.js
+++ tests/specs/sass.mocha.spec.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
-var path     = require('path'),
-    sassTrue = require('sass-true'),
-    sassFile = path.join(__dirname, 'node-tests.scss');
+var path     = require("path"),
+    sassTrue = require("sass-true"),
+    sassFile = path.join(__dirname, "node-tests.scss");
 
 sassTrue.runSass({file: sassFile}, describe, it);
